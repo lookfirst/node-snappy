1959901bee490f644d8c50666763989c35503732
Karl Skomski <karl@skomski.com>
2012-03-09 22:31:56 +0100

    Prettier tests

--- test/test.js
+++ test/test.js
@@ -4,34 +4,34 @@ var Fs     = require('fs');
 
 var tests = 0;
 
-test = function(buffer) {
+test = function(name, buffer) {
   Snappy.compress(buffer, function(err, compressed){
     Assert.ifError(err);
 
     Snappy.decompress(compressed, function(err, decompressed){
       Assert.ifError(err);
       Assert.equal(decompressed.toString(), buffer.toString());
-      tests++
+      console.log('ok ' + ++tests + ' ' + name);
     });
   });
 }
 
-test(new Buffer('spppppppppassssssssssssssspuuuuuuuuuuur!!___"DSA'));
-test(new Buffer([255, 200, 100, 3, 0, 256, 80]));
-test(new Buffer('काक्नोम्यत्क्नोम्यत्चं शक्नोम्यत्तुमतुम् ।तुम् ।् । नोपहिनस्ति माम् ॥'));
-test(new Buffer(JSON.stringify({
+test('simple', new Buffer('spppppppppassssssssssssssspuuuuuuuuuuur!!___"DSA'));
+test('raw', new Buffer([255, 200, 100, 3, 0, 256, 80]));
+test('utf8', new Buffer('काक्नोम्यत्क्नोम्यत्चं शक्नोम्यत्तुमतुम् ।तुम् ।् । नोपहिनस्ति माम् ॥'));
+test('json', new Buffer(JSON.stringify({
   status: 404,
   errorName: 'NoSuchBucket',
   errorMessage: 'The specified bucket does not exist.',
   uuid: '550e8400-e29b-41d4-a716-4466554400002'
 })));
-test(new Buffer(JSON.stringify({
+test('json2', new Buffer(JSON.stringify({
   uuid: 'd758be63-3f8',
   data: {
     'ddddddddd': 'yyyyyyyyyyyyyyyyyyyy'
   }
 })));
-test(Fs.readFileSync(__dirname + '/urls.10K'));
+test('file', Fs.readFileSync(__dirname + '/urls.10K'));
 
 process.on('exit', function() {
   Assert.equal(tests, 6);
